830490c4aa145d61fedc6f203f63349ccf752ff9
Tom <tom@something.com>
2017-01-01 19:57:16 +0100

    new commit

---
+++ assignment4/app/view1/items.component.js
@@ -0,0 +1,3 @@
+/**
+ * Created by tomaszosuchowski on 1/1/17.
+ */
---
+++ assignment4/app/view1/menudata.service.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tomaszosuchowski on 1/1/17.
+ */
+
+'use strict';
+
+angular.module('module', ['ngRoute'])
+.service('MenuDataService', function() {
+
+    var getAllCategories = function (http){
+
+    }
+var getItemsForCategory= function(categoryShortName){
+
+
+        }
+    }
