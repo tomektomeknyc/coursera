8ea1909ef4789477d46d0a0d10ef696e771fdc55
Tom <tom@something.com>
2017-01-09 20:42:11 +0100

    New commit

---
+++ Assignment_5/src/common/myinfo.service.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict";
+
+    angular.module('common').service('MyInfoService', MyInfoService);
+
+    MyInfoService.$inject = [];
+
+    function MyInfoService() {
+
+        var service = this;
+
+        service.setInfo = function (info) {
+            service.info = info;
+        };
+
+        service.getInfo = function () {
+            return service.info;
+        }
+    };
+})();
